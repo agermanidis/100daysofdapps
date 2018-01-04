82bd17f1c006af0858160aa8c77b845efd118ccf
Anastasis Germanidis <agermanidis@gmail.com>
2018-01-04 10:32:31 -0500

    routing fixes

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Route, IndexRoute } from "react-router";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { HashRouter, Switch } from "react-router-dom";
 
 import { EthereumWrapper } from "./common";
 
 import Day001 from "./001_Friendship";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/001_Friendship" component={Day001} />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
